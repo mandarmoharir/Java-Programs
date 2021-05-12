d6d2e0542112325cce35cea0b6f1d2cf54531762
Mandar Moharir <moharirmandar01@gmail.com>
2021-05-12 20:46:19 +0530

    First hello world java program..!!! First ever commit to git from personal device to personally created repo.

--- src/voidMain/mainmethod.java
+++ src/voidMain/mainmethod.java
@@ -4,5 +4,6 @@ public class mainmethod {
 
     public static void main(String[]args){
         System.out.println("Hello World....!!!");
+        System.out.println("New line to check git activity..!!");
     }
 }
